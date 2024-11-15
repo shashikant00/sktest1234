20c2f774d05c46cc6e6b6580ffd48ef1738f0c44
shashikant00 <shashikantgit10@gmail.com>
2024-11-15 22:38:09 +0530

    second commit

--- SpringBootApp1/src/main/java/com/coursecube/springboot/controller/HomeController.java
+++ SpringBootApp1/src/main/java/com/coursecube/springboot/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.coursecube.springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	
+	@GetMapping("/")
+	public String happy() {
+		return "Hello Happy";
+	}
 }
